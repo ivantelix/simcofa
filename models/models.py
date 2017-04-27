12e5941bf60b808b2736aff2e699937a83e1c474
ivantelix <ivantelix@gmail.com>
2017-04-27 07:16:40 -0400

    Primer Commit, creando entorno de trabajo y Creado del primer modelo

--- models/models.py
+++ models/models.py
@@ -6,20 +6,4 @@ class Empresa(models.Model):
 
 	rif = fields.Char(string="rif", size=10, required=True, unique=True, help='Ingrese el rif EJ:j204875366')
 	nombre = fields.Char(size=100, required=True, unique=True, help='Razon Social de la Empresa')
-	fecha_ini = fields.DateTime(auto_now=True)
-	fecha_act = fields.DateTime(auto_now=False, required=False, default=None)
-
-	#usuario_ini
-	#usuario_act
-
-# class simcofa-coop(models.Model):
-#     _name = 'simcofa-coop.simcofa-coop'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+	fecha_ini = fields.Datetime(auto_now=True)	
